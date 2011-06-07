cb06a35efbebc0cee1f514c31b71681411c6f5c0
MATSUMOTO Yasutaka <matsumoto@komatsuelec.co.jp>
2011-06-07 18:31:31 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cromos::Application.config.secret_token = '9672ef4b355e70126ff0b0260f773490e2c8e251f9e749e91534cc5001a2c2595de7bdb1ef97f435d0b71ac7970cfa4ddf19107e7baae15f5dbea683864a96d2'
